c584a16a8f6a95511410e7d10c1156567070ac5a
thisWeb1225 <shan881225@gmail.com>
2024-03-31 17:28:50 +0800

    ✨ Add Clerk

---
+++ app/(platform)/layout.tsx
@@ -0,0 +1,19 @@
+// import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
+
+// import { ModalProvider } from "@/components/providers/modal-provider";
+// import { QueryProvider } from "@/components/providers/query-provider";
+
+const PlatformLayout = ({
+  children
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <ClerkProvider>
+        {children}
+    </ClerkProvider>
+  );
+};
+
+export default PlatformLayout;
